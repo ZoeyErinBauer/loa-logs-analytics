1c621465f300e45b9969f0479f7220a079ee9740
Zoey <zbauer@checkintocash.com>
2023-12-04 23:29:45 -0500

    cleaned up mod issue resolved cargo build problem

--- src-tauri/src/analytics/models.rs
+++ src-tauri/src/analytics/models.rs
@@ -1,3 +1,6 @@
+use hashbrown::HashMap;
+use serde::{Deserialize, Serialize};
+
 #[derive(Default,Debug,Clone,PartialEq,Serialize,Deserialize)]
 pub struct UptimeGraphData{
     pub data_points : HashMap<String,f32>
