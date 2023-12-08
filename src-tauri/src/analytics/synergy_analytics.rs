b7e50e40a0f837a195d6047d542634ddd0b82464
Zoey <zbauer@checkintocash.com>
2023-12-08 00:49:12 -0500

    added more query logic

--- src-tauri/src/analytics/synergy_analytics.rs
+++ src-tauri/src/analytics/synergy_analytics.rs
@@ -4,11 +4,25 @@ use hashbrown::HashMap;
 use crate::analytics::models::{UptimeAnalytics, UptimeGraphData};
 use crate::get_db_connection;
 
-fn calculate_synergy_uptime(character_name: String, start_date: NaiveDate, end_date: NaiveDate, resource_path: &Path ) -> UptimeAnalytics
+fn calculate_synergy_uptime(character_name: String, start_date: NaiveDate, end_date: NaiveDate, resource_path: &Path) -> UptimeAnalytics
 {
-
     let conn = get_db_connection(resource_path).expect("could not get db connection");
-    //Use connection to build sql query here to get data out for expected entity
+    //potentially optimize the join in reverse to use encounters instead of characters
+    // first have to check if last_update stores the information on placed date.
+    let query = format!("Select
+    e.skill_stats,
+    e.name,
+    e.encounter_id,
+    e.last_update
+    FROM entity e
+    where e.name LIKE '%'{}'%'", character_name);
+
+    let mut entity_stmt = conn.prepare_cached(&query).unwrap();
+    let player_rows = entity_stmt.query([]);
+    while let Some(row) = player_rows.next()?
+    {
+        //ITERATE OVER ENTITY ROWS SORT FILTER ENCOUNTER DATA OUT FOR EACH DATE
+    }
     let mut data_points = HashMap::new();
     let mut graph_data = UptimeGraphData {
         data_points
